1c12eeea1493c797772d4ed5c2d4c1808791c054
Arthur Quinn <artquint@rutgers.edu>
2017-04-07 22:18:30 -0400

    changed chart titles

--- TestPoly.m
+++ TestPoly.m
@@ -17,15 +17,15 @@ function TestPoly(n)
     plot(X,Y,'r--','linewidth',2);hold on; grid on;
     plot(X,C,'k-','linewidth',2);hold on; grid on;
     plot(X,L,'b-','linewidth',2);hold on; grid on;
-    set(gca,'fontsize',20);xlabel('x');ylabel('y');
-    title(['polynomial, n = ' num2str(n)]);
+    set(gca,'fontsize',20);xlabel('x');ylabel('f(x)');
+    title(['f(x) = exp(sin(5x)), n = ' num2str(n)]);
     legend('f(x)','c(x)','p(x)');
     
     figure;
     plot(X,abs(Y-C),'k-','linewidth',2);hold on; grid on;
     plot(X,abs(Y-L),'b-','linewidth',2);hold on; grid on;
-    set(gca,'fontsize',20);xlabel('x');ylabel('y');
-    title(['polynomial, n = ' num2str(n)]);
+    set(gca,'fontsize',20);xlabel('x');ylabel('Abs error');
+    title(['f(x) = exp(sin(5x)), n = ' num2str(n)]);
     legend('|f(x)-c(x)|','|f(x)-p(x)|');
     
     err = [max(abs(f(X)-C)) max(abs(f(X)-L)) sqrt(mean((f(X)-C).^2)) sqrt(mean((f(X)-L).^2))]
